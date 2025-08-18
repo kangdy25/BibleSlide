574c72c3fd33fd54f425a7fe861968bda79dc5f3
Shineast <canfrog@naver.com>
2025-08-18 22:17:07 +0900

    🔧 [Chore] Electron, TypeScript, Vite, React 개발 환경 세팅

--- app/preload.ts
+++ app/preload.ts
@@ -1,6 +1,7 @@
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld("electronAPI", {
   fetchVerse: (input: string) => ipcRenderer.invoke("fetch-verse", input),
+  ping: () => ipcRenderer.invoke("ping"),
   generatePpt: (input: string) => ipcRenderer.invoke("generate-ppt", input), // 기존 PPT용도
 });
