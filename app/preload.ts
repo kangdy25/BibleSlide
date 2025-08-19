76ce7c83f1faf7f92b09179b0691441dc7c7f37f
Shineast <canfrog@naver.com>
2025-08-19 14:37:40 +0900

    🛠 [Chore] package.json의 script 수정 (ts 파일 컴파일 옵션 추가)
    - package.json 내부의 dev 스크립트에 app 폴더 하위의 ts 파일 컴파일 및 watch 추가

--- app/preload.ts
+++ app/preload.ts
@@ -1,5 +1,7 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
+console.log(1 + 2);
+
 contextBridge.exposeInMainWorld("electronAPI", {
   fetchVerse: (input: string) => ipcRenderer.invoke("fetch-verse", input),
   ping: () => ipcRenderer.invoke("ping"),
