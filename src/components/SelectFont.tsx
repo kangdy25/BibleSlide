c2a9d4f4989df605e3e73c01bac90fba99f373cb
Kangdy <canfrog@naver.com>
2025-08-21 01:05:26 +0900

    ✨ [Feat] Font Dropdown 분리 및 컴포넌트화

---
+++ src/components/SelectFont.tsx
@@ -0,0 +1,42 @@
+import styles from './SelectFont.module.css';
+
+import * as Select from "@radix-ui/react-select"
+import { ChevronDown, Check } from "lucide-react"
+import { Dispatch, SetStateAction } from 'react';
+
+export interface SelectFontProps {
+  font: string;
+  setFont: Dispatch<SetStateAction<string>>;
+}
+
+const SelectFont = ({font, setFont}: SelectFontProps) => {
+  return (
+    <div className={styles.section}>
+              <label className={styles.sectionLabel}>폰트</label>
+              <Select.Root value={font} onValueChange={setFont}>
+                <Select.Trigger className={styles.selectTrigger}>
+                  <Select.Value />
+                  <Select.Icon className={styles.selectIcon}>
+                    <ChevronDown />
+                  </Select.Icon>
+                </Select.Trigger>
+                <Select.Portal>
+                  <Select.Content className={styles.selectContent}>
+                    <Select.Viewport className={styles.selectViewport}>
+                      {["Pretendard", "Noto Sans KR", "Sans-serif", "Serif"].map((fontOption) => (
+                        <Select.Item key={fontOption} className={styles.selectItem} value={fontOption}>
+                          <Select.ItemText>{fontOption}</Select.ItemText>
+                          <Select.ItemIndicator className={styles.selectItemIndicator}>
+                            <Check />
+                          </Select.ItemIndicator>
+                        </Select.Item>
+                      ))}
+                    </Select.Viewport>
+                  </Select.Content>
+                </Select.Portal>
+              </Select.Root>
+            </div>
+  )
+}
+
+export default SelectFont
--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,10 +1,8 @@
 import styles from './Sidebar.module.css'
 import { Dispatch, SetStateAction } from "react";
-
-import * as Select from "@radix-ui/react-select"
-import { ChevronDown, Check } from "lucide-react"
 import SearchInput from '../components/SearchInput';
 import TextSlider from '../components/TextSlider';
+import SelectFont from '../components/SelectFont';
 
 export interface SidebarProps {
   verseInput: string;
@@ -44,35 +42,16 @@ const Sidebar = ({
   return (
     <div className={styles.sidebar}>
           <div className={styles.sidebarContent}>
+            {/* PPT로 제작할 성경을 입력 받을 Input */}
             <SearchInput verseInput={verseInput} setVerseInput={setVerseInput} />
+            
+            {/* 글자 옵션을 정하는 Slider */}
             <TextSlider {...allTextSliderProps} />
 
-            <div className={styles.section}>
-              <label className={styles.sectionLabel}>폰트</label>
-              <Select.Root value={font} onValueChange={setFont}>
-                <Select.Trigger className={styles.selectTrigger}>
-                  <Select.Value />
-                  <Select.Icon className={styles.selectIcon}>
-                    <ChevronDown />
-                  </Select.Icon>
-                </Select.Trigger>
-                <Select.Portal>
-                  <Select.Content className={styles.selectContent}>
-                    <Select.Viewport className={styles.selectViewport}>
-                      {["Pretendard", "Noto Sans KR", "Sans-serif", "Serif"].map((fontOption) => (
-                        <Select.Item key={fontOption} className={styles.selectItem} value={fontOption}>
-                          <Select.ItemText>{fontOption}</Select.ItemText>
-                          <Select.ItemIndicator className={styles.selectItemIndicator}>
-                            <Check />
-                          </Select.ItemIndicator>
-                        </Select.Item>
-                      ))}
-                    </Select.Viewport>
-                  </Select.Content>
-                </Select.Portal>
-              </Select.Root>
-            </div>
-            
+            {/* 폰트를 지정할 드롭다운 */}
+            <SelectFont font={font} setFont={setFont} />
+
+            {/* PPT 제작 버튼 */}
             <div className={styles.exportSection}>
               <button className={styles.exportButton}>
                 <span>PPT 제작하기</span>
