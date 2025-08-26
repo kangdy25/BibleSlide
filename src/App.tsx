00cb829ce7d6c9a3bfcfcb7043a2fec02be5af23
Shineast <canfrog@naver.com>
2025-08-26 20:57:46 +0900

    ♻️ [Refactor] SelectFont 컴포넌트가 전역 상태를 사용하도록 리팩토링
    - 불필요한 변수 및 console.log 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import Header from './ui/Header';
 import Sidebar from './ui/Sidebar';
 import Content from './ui/Content';
 
 function App() {
-  const [font, setFont] = useState('Pretendard');
-
-  const allSidebarProps = {
-    font,
-    setFont,
-  };
-
   return (
     <div className="container">
       <Header />
       <div className="mainContent">
-        <Sidebar {...allSidebarProps} />
+        <Sidebar />
         <Content />
       </div>
     </div>
