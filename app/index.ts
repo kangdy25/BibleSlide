de2fb6c92dff1f7ca282d837c841695a45cc359a
Shineast <canfrog@naver.com>
2025-08-28 19:37:07 +0900

    ✨ [Feat] PPT 생성 시 폰트 설정, 텍스트 크기, 자간, 행간 적용

--- app/index.ts
+++ app/index.ts
@@ -49,54 +49,76 @@ ipcMain.handle('fetch-verse', (event, input: string, version: string = 'GAE') =>
   }
 });
 
-ipcMain.handle('generate-slide', async (event, data: { input: string; bibleVersion: string }) => {
-  try {
-    const { input, bibleVersion } = data;
-    const verses = fetchVerses(input, bibleVersion);
-
-    let pptx: PptxGenJS;
-
-    verses.forEach((verse, idx) => {
-      const title = `${verse.split(':')[1]} ${verse.split(':')[3]}장 ${verse.split(':')[4]}절`;
-      const subTitle = `${verse.split(':')[1]} ${verse.split(':')[3]}장`;
-      const engTitle = `${verse.split(':')[2]} ${verse.split(':')[3]}:${verse.split(':')[4]}`;
-      const engSubTitle = `${verse.split(':')[2]} ${verse.split(':')[3]}`;
-      const verseContent = `${verse.split(':')[5]}`;
-
-      if (bibleVersion === 'KJV' || bibleVersion === 'NIV') {
-        // 영어 버전인 경우, 제목과 소제목을 영어로 구성
-        pptx = generatePPT(engTitle, engSubTitle, verseContent, pptx);
-      } else {
-        // pptx가 undefined이면 새로 생성, 있으면 슬라이드 추가
-        pptx = generatePPT(title, subTitle, verseContent, pptx);
-      }
-    });
-
-    // 파일 저장 다이얼로그
-    const saveFileName = input.replace(/^([가-힣a-zA-Z]+)\s*(\d+)[:|-](.*)$/, '$1$2장$3절');
-
-    const { filePath } = await dialog.showSaveDialog({
-      title: 'Save PowerPoint File',
-      defaultPath: `${saveFileName}.pptx`,
-      filters: [{ name: 'PowerPoint', extensions: ['pptx'] }],
-    });
-
-    if (filePath) {
-      await pptx!.writeFile({ fileName: filePath });
-      return { success: true, message: `File saved to ${filePath}` };
+ipcMain.handle(
+  'generate-slide',
+  async (
+    event,
+    data: {
+      input: string;
+      bibleVersion: string;
+      font: string;
+      textSize: number;
+      letterSpacing: number;
+      lineHeight: number;
     }
+  ) => {
+    try {
+      const { input, bibleVersion, font, textSize, letterSpacing, lineHeight } = data;
+      const verses = fetchVerses(input, bibleVersion);
 
-    return { success: false, message: 'File save cancelled' };
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      console.error('Error generating slide:', err.message);
-      return { success: false, message: `Error: ${err.message}` };
-    } else {
-      console.error('Unexpected error:', err);
-      return { success: false, message: 'Unexpected error occurred' };
+      let pptx: PptxGenJS;
+
+      verses.forEach((verse, idx) => {
+        const title = `${verse.split(':')[1]} ${verse.split(':')[3]}장 ${verse.split(':')[4]}절`;
+        const subTitle = `${verse.split(':')[1]} ${verse.split(':')[3]}장`;
+        const engTitle = `${verse.split(':')[2]} ${verse.split(':')[3]}:${verse.split(':')[4]}`;
+        const engSubTitle = `${verse.split(':')[2]} ${verse.split(':')[3]}`;
+        const verseContent = `${verse.split(':')[5]}`;
+
+        if (bibleVersion === 'KJV' || bibleVersion === 'NIV') {
+          // 영어 버전인 경우, 제목과 소제목을 영어로 구성
+          pptx = generatePPT(
+            engTitle,
+            engSubTitle,
+            verseContent,
+            font,
+            textSize,
+            letterSpacing,
+            lineHeight,
+            pptx
+          );
+        } else {
+          // pptx가 undefined이면 새로 생성, 있으면 슬라이드 추가
+          pptx = generatePPT(title, subTitle, verseContent, font, textSize, letterSpacing, lineHeight, pptx);
+        }
+      });
+
+      // 파일 저장 다이얼로그
+      const saveFileName = input.replace(/^([가-힣a-zA-Z]+)\s*(\d+)[:|-](.*)$/, '$1$2장$3절');
+
+      const { filePath } = await dialog.showSaveDialog({
+        title: 'Save PowerPoint File',
+        defaultPath: `${saveFileName}.pptx`,
+        filters: [{ name: 'PowerPoint', extensions: ['pptx'] }],
+      });
+
+      if (filePath) {
+        await pptx!.writeFile({ fileName: filePath });
+        return { success: true, message: `File saved to ${filePath}` };
+      }
+
+      return { success: false, message: 'File save cancelled' };
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error('Error generating slide:', err.message);
+        return { success: false, message: `Error: ${err.message}` };
+      } else {
+        console.error('Unexpected error:', err);
+        return { success: false, message: 'Unexpected error occurred' };
+      }
     }
   }
-});
+);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
